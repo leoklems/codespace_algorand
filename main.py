3f72810719370348bddeedfd4b7ce4e07297cf3d
Obumneme Clement <62294408+leoklems@users.noreply.github.com>
2024-09-20 01:04:36 +0000

    started

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from algokit_utils.beta.algorand_client import (
+    AlgorandClient,
+    AssetCreateParams,
+    AssetOptInParams,
+    AssetTransferParams,
+    PayParams,
+)
 
+algorand = AlgorandClient.default_local_net()
+# Dispenser funds your account initially
+dispenser = algorand.account.dispenser()
+# KMD is key management deamon which is like your wallet
+print(dispenser.address)
